4375cdccb3a6b29016863a3ab5ddf94e95e3f821
nikakarumidze <nikakarumidze@gmail.com>
2024-02-19 21:55:42 +0400

    integrated singleton pattern

--- backend/repository/DBConnection.py
+++ backend/repository/DBConnection.py
@@ -1,9 +1,14 @@
 import sqlite3
 
 class DBConnection:
-    def __init__(self):
-        self.conn = sqlite3.connect('Bank_database.db')
-        self.cursor = self.conn.cursor()
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.conn = sqlite3.connect('Bank_database.db')
+            cls._instance.cursor = cls._instance.conn.cursor()
+        return cls._instance
 
     def create_tables(self):
         with open(r'C:\Users\nikak\bank\backend\init.sql', 'r') as file:
